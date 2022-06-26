2fe70b13a53a406402dab86ba07cafbe5ce81c32
raymondehizoba <rayehizoba@gmail.com>
2022-06-26 22:24:23 +0000

    update serviceseeder

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -18,24 +18,28 @@ class ServiceSeeder extends Seeder
         Service::create([
             'heading' => 'We develop',
             'name' => 'Website',
+            'slug' => 'website',
             'lottie_src' => 'https://assets10.lottiefiles.com/packages/lf20_4VvdIQ.json',
         ]);
 
         Service::create([
             'heading' => 'We build',
             'name' => 'Mobile App',
+            'slug' => 'mobile-app',
             'lottie_src' => 'https://assets6.lottiefiles.com/packages/lf20_r6lfrga3.json',
         ]);
 
         Service::create([
             'heading' => 'We make',
             'name' => '3D Design',
+            'slug' => '3d-design',
             'lottie_src' => 'https://assets6.lottiefiles.com/packages/lf20_ihg7zmol.json',
         ]);
 
         Service::create([
             'heading' => 'We do',
             'name' => '3D Printing',
+            'slug' => '3d-printing',
             'lottie_src' => 'https://assets6.lottiefiles.com/packages/lf20_vu2p4il8.json',
         ]);
     }
